a603628425761c5ce7bb858f98a331a6cc6a3e0a
SENEAss <144698610+SENEAss@users.noreply.github.com>
2024-10-29 10:13:48 +0100

    refreshing  maintains the active tab

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [PM2_5Data, setPM2_5Data] = useState([]);
   const [PM10Data, setPM10Data] = useState([]);
 
-  const [currentMenu, setCurrentMenu] = useState('dashboard');
+  const [currentMenu, setCurrentMenu] = useState(localStorage.getItem('currentMenu') || 'dashboard');
   const [notifications, setNotifications] = useState([]);
   const [alertCount, setAlertCount] = useState(0);
   const [alertSent, setAlertSent] = useState({ temperature: false, CO2: false, TVOC: false, sound: false });
@@ -30,7 +30,7 @@ function App() {
   };
 
   const addNotification = (message) => {
-    const timestamp = new Date().toLocaleString(); // Ajouter la date et l'heure de la notification
+    const timestamp = new Date().toLocaleString();
     setNotifications((prev) => [...prev, { ...message, timestamp, read: false }]);
     setAlertCount((prev) => prev + 1);
   };
@@ -102,6 +102,10 @@ function App() {
     const interval = setInterval(fetchData, 2000);
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('currentMenu', currentMenu);
+  }, [currentMenu]);
 
   const handleMenuClick = (menu) => {
     setCurrentMenu(menu);
