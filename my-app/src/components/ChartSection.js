520c3ffbf7f306184777926651f3ea9ca7af7b9d
aymericbarbot <aymeric.barbot@neusta.fr>
2024-11-28 14:42:36 +0100

    mise en echel 24h point

--- my-app/src/components/ChartSection.js
+++ my-app/src/components/ChartSection.js
@@ -127,84 +127,224 @@ const ChartSection = ({
   return (
     <Grid container spacing={3} style={{ marginTop: 20 }}>
       {/* Graphique combiné avec checkboxes */}
-      <Grid item xs={10}>
+      <Grid container spacing={3} style={{ marginTop: 20 }}>
+  {/* Graphique combiné avec checkboxes */}
+  <Grid item xs={10}>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={{
+        chart: {
+          type: 'spline',
+          backgroundColor: '#2E2F45',
+          height: 465,
+          zoomType: 'x', // Permet de zoomer sur l'axe des x
+          resetZoomButton: {
+            position: {
+              align: 'right', // Positionner à droite
+              verticalAlign: 'top', // Positionner en haut
+              x: -10,
+              y: 10,
+            },
+            theme: {
+              fill: '#fff',
+              stroke: '#000',
+              style: {
+                color: '#000',
+              },
+              r: 5,
+              states: {
+                hover: {
+                  fill: '#f7f7f7',
+                  style: {
+                    color: '#333',
+                  },
+                },
+              },
+            },
+          },
+        },
+        title: {
+          text: 'Combined Data',
+          style: { color: '#fff' },
+          align: 'center',
+        },
+        xAxis: {
+          type: 'datetime',
+          gridLineWidth: 1,
+          gridLineColor: '#fff',
+          title: { text: 'Time', style: { color: '#fff' } },
+          labels: { style: { color: '#fff' }, format: '{value:%H:%M}' },
+          min: (() => {
+            const todayDate = new Date();
+            return todayDate.setHours(0, 0, 0, 0); // Début de la journée
+          })(),
+          max: (() => {
+            const todayDate = new Date();
+            return todayDate.setHours(24, 0, 0, 0); // Fin de la journée
+          })(),
+          // Suppression de tickInterval pour un ajustement dynamique par Highcharts
+        },
+        yAxis: {
+          title: { text: 'Values', style: { color: '#fff' } },
+          gridLineWidth: 1,
+          gridLineColor: '#fff',
+          labels: { style: { color: '#fff' } },
+          min: 0,
+        },
+        series: [
+          visibleSeries.temperature && { name: 'Temperature', data: filterDataPerMinute(temperatureData), color: '#FF0000' },
+          visibleSeries.humidity && { name: 'Humidity', data: filterDataPerMinute(humidityData), color: '#00FF00' },
+          visibleSeries.CO2 && { name: 'CO2', data: filterDataPerMinute(CO2Data), color: '#0000FF' },
+          visibleSeries.TVOC && { name: 'TVOC', data: filterDataPerMinute(TVOCData), color: '#FFFF00' },
+          visibleSeries.sound && { name: 'Sound', data: filterDataPerMinute(soundData), color: '#f033ff' },
+          visibleSeries.PM1_0 && { name: 'PM1.0', data: filterDataPerMinute(PM1_0Data), color: '#8E44AD' },
+          visibleSeries.PM2_5 && { name: 'PM2.5', data: filterDataPerMinute(PM2_5Data), color: '#16A085' },
+          visibleSeries.PM10 && { name: 'PM10', data: filterDataPerMinute(PM10Data), color: '#F39C12' },
+        ].filter(Boolean),
+        tooltip: {
+          shared: true,
+          valueDecimals: 2,
+        },
+      }}
+    />
+  </Grid>
+
+  {/* Checkboxes pour le graphique combiné */}
+  <Grid item xs={2}>
+    <FormGroup
+      style={{
+        marginLeft: '50px', // Décale les checkboxes vers la droite
+        padding: '30px', // Ajoute un peu d'espace intérieur
+        backgroundColor: '#2E2F45', // Optionnel, ajoute un fond pour démarquer
+        borderRadius: '8px', // Bordures arrondies
+      }}
+    >
+      {Object.keys(visibleSeries).map((key) => (
+        <FormControlLabel
+          key={key}
+          control={
+            <Checkbox
+              checked={visibleSeries[key]}
+              onChange={handleCheckboxChange}
+              name={key}
+              style={{ color: '#fff' }} // Couleur des cases
+            />
+          }
+          label={key.charAt(0).toUpperCase() + key.slice(1)}
+          style={{
+            color: '#fff', // Couleur du texte
+            marginBottom: '8px', // Réduit l'espacement entre les lignes
+            width: 'auto', // Réduit la largeur des lignes
+          }}
+        />
+      ))}
+    </FormGroup>
+  </Grid>
+</Grid>
+
+      {/* Graphique de l'humidex */}
+      <Grid item xs={10} style={{ marginTop: '20px' }}>
         <HighchartsReact
           highcharts={Highcharts}
           options={{
             chart: {
               type: 'spline',
               backgroundColor: '#2E2F45',
-              height: 465,
+              height: 400,
+              zoomType: 'x', // Ajout pour permettre de zoomer sur l'axe des x
+              resetZoomButton: {
+                position: {
+                  align: 'right', // Positionner à droite
+                  verticalAlign: 'top', // Positionner en haut
+                  x: -10,
+                  y: 10,
+                },
+                theme: {
+                  fill: '#fff',
+                  stroke: '#000',
+                  style: {
+                    color: '#000',
+                  },
+                  r: 5,
+                  states: {
+                    hover: {
+                      fill: '#f7f7f7',
+                      style: {
+                        color: '#333',
+                      },
+                    },
+                  },
+                },
+              },
             },
             title: {
-              text: 'Combined Data',
+              text: 'Humidex Over Time',
               style: { color: '#fff' },
               align: 'center',
             },
             xAxis: {
               type: 'datetime',
-              gridLineWidth: 1, // Ajout de grilles verticales
-              gridLineColor: '#fff', // Couleur de la grille
+              gridLineWidth: 1,
+              gridLineColor: '#fff',
               title: { text: 'Time', style: { color: '#fff' } },
               labels: { style: { color: '#fff' }, format: '{value:%H:%M}' },
+              min: (() => {
+                const todayDate = new Date();
+                return todayDate.setHours(0, 0, 0, 0); // Début de la journée
+              })(),
+              max: (() => {
+                const todayDate = new Date();
+                return todayDate.setHours(24, 0, 0, 0); // Fin de la journée
+              })(),
+              plotLines: humidexData.length > 0 ? [
+                {
+                  color: '#4840d6',
+                  width: 2,
+                  value: humidexData[humidexData.length - 1][0],
+                  zIndex: 2,
+                  dashStyle: 'Dash',
+                  label: {
+                    text: 'Last Data Point',
+                    rotation: 0,
+                    y: 20,
+                    style: { color: '#fff' },
+                  },
+                },
+              ] : [],
             },
             yAxis: {
+              min: 0,
+              max: 100,
+              tickInterval: 10,
               title: { text: 'Values', style: { color: '#fff' } },
-              gridLineWidth: 1, // Ajout de grilles horizontales
-              gridLineColor: '#fff', // Couleur de la grille
               labels: { style: { color: '#fff' } },
             },
             series: [
-              visibleSeries.temperature && { name: 'Temperature', data: temperatureData, color: '#FF0000' },
-              visibleSeries.humidity && { name: 'Humidity', data: humidityData, color: '#00FF00' },
-              visibleSeries.CO2 && { name: 'CO2', data: CO2Data, color: '#0000FF' },
-              visibleSeries.TVOC && { name: 'TVOC', data: TVOCData, color: '#FFFF00' },
-              visibleSeries.sound && { name: 'Sound', data: soundData, color: '#00000F' },
-              visibleSeries.PM1_0 && { name: 'PM1.0', data: PM1_0Data, color: '#8E44AD' },
-              visibleSeries.PM2_5 && { name: 'PM2.5', data: PM2_5Data, color: '#16A085' },
-              visibleSeries.PM10 && { name: 'PM10', data: PM10Data, color: '#F39C12' },
-            ].filter(Boolean),
+              {
+                name: 'Humidex',
+                data: filterDataPerMinute(humidexData),
+                lineWidth: 4,
+                marker: {
+                  lineWidth: 2,
+                  lineColor: '#4840d6',
+                  fillColor: '#fff',
+                  enabled: true,
+                  radius: 4,
+                },
+                color: {
+                  linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                  stops: [
+                    [0, '#fa4fed'],
+                    [1, '#5897ff'],
+                  ],
+                },
+              },
+            ],
+            tooltip: {
+              shared: true,
+              valueDecimals: 2,
+            },
           }}
-        />
-      </Grid>
-
-      {/* Checkboxes pour le graphique combiné */}
-      <Grid item xs={2}>
-          <FormGroup
-            style={{
-              marginLeft: '50px', // Décale les checkboxes vers la droite
-              padding: '30px', // Ajoute un peu d'espace intérieur
-              backgroundColor: '#2E2F45', // Optionnel, ajoute un fond pour démarquer
-              borderRadius: 'px', // Bordures arrondies
-            }}
-          >
-            {Object.keys(visibleSeries).map((key) => (
-              <FormControlLabel
-                key={key}
-                control={
-                  <Checkbox
-                    checked={visibleSeries[key]}
-                    onChange={handleCheckboxChange}
-                    name={key}
-                    style={{ color: '#fff' }} // Couleur des cases
-                  />
-                }
-                label={key.charAt(0).toUpperCase() + key.slice(1)}
-                style={{
-                  color: '#fff', // Couleur du texte
-                  marginBottom: '8px', // Réduit l'espacement entre les lignes
-                  width: 'auto', // Réduit la largeur des lignes
-                }}
-              />
-            ))}
-          </FormGroup>
-        </Grid>
-
-      {/* Graphique de l'humidex */}
-      <Grid item xs={10} style={{ marginTop: '20px' }}>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={options('Humidex Over Time', humidexData, null, true)}
         />
       </Grid>
     </Grid>
